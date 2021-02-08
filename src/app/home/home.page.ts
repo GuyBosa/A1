aa1ce80180169bfb9a003745a345666f5d678db9
GuyBosa1 <guyb@xfour.co.za>
2021-02-08 16:44:19 +0200

    initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public http: HttpClient, public loadingController: LoadingController  ) {
+    this.getUsersList()
+    this.getMessage()
+  }
+
+  //declaring array 
+  usersList:any[]=[]
+  things:any;
+
+  //Api call to reload displayed user data
+  async getUsersList()
+  {
+    //loader to compensate for network speed
+    const loading = await this.loadingController.create({
+      message: 'Please wait . . .',
+      duration: 2000
+    });
+    await loading.present();
+
+    this.http.get('https://randomuser.me/api/?results=1').subscribe(data=>{
+      this.usersList=data["results"]
+      loading.dismiss()
+      console.log(data)
+    })
+  }
+
+  async getMessage(){
+    //loader to compensate for network speed
+    const loading = await this.loadingController.create({
+      message: 'Please wait . . .',
+      duration: 2000
+    });
+    await loading.present();
+    
+    this.http.get('http://ec2-34-249-218-118.eu-west-1.compute.amazonaws.com:3000').subscribe(stuff=>{
+      this.things=stuff["message"]
+      console.log(stuff)
+    })
+  }
+
+
+}
